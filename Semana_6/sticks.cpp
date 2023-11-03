e7b5e47f891892c0bfa0a468db1c0830b732e52c
Micael <mikal2003.sp@gmail.com>
2023-11-03 17:00:17 -0300

    fazendo deteccao de ciclos para exercicio q usa TOPO

--- Semana_6/sticks.cpp
+++ Semana_6/sticks.cpp
@@ -10,7 +10,7 @@
 
 // input
 // o input consiste em varios casos testes
-// a primeira linha de caso de teste contem
+// a primeira linha- de caso de teste contem
 // 2 inteiros n e m
 // n eh o numero de palitos
 // e m eh o proximo numero de linhas
@@ -29,22 +29,58 @@
 #include <iostream>
 #include <vector>
 
-void dfs(std::vector<std::vector<int>> &adjs, int u, std::vector<int>&ordem, std::vector<bool> &visited) {
+enum class State { NOT_VISITED, TEMPORARY_VISITED, PERMANENTLY_VISITED };
+
+bool hasCycleDFS(std::vector<std::vector<int>>& graph, std::vector<State>& visited, int u) {
+    visited[u] = State::TEMPORARY_VISITED;
+
+    for (int v = 0; v < graph.size(); v++) {
+        if (graph[u][v] == 0) continue;
+        if (visited[v] == State::TEMPORARY_VISITED) {
+            // Encontrou um vértice já visitado temporariamente, indicando um ciclo.
+            return true;
+        } else if (visited[v] == State::NOT_VISITED) {
+            if (hasCycleDFS(graph, visited, v)) {
+                return true;
+            }
+        }
+    }
+
+    visited[u] = State::PERMANENTLY_VISITED;
+    return false;
+}
+
+bool hasCycle(std::vector<std::vector<int>>& graph) {
+    int n = graph.size();
+    std::vector<State> visited(n, State::NOT_VISITED);
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == State::NOT_VISITED) {
+            if (hasCycleDFS(graph, visited, i)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+void DFS_Visit_OT(std::vector<std::vector<int>>& adjs, int u, std::vector<bool>& visited, std::vector<int>& ordem){
     visited[u] = true;
-    for (int v = 0; v < adjs.size(); v++) {
-        if (adjs[u][v] == 1 && !visited[v]) {
-            dfs(adjs, v, ordem, visited);
+    for(int v = 0; v < adjs.size(); v++){
+        if(adjs[u][v] && !visited[v]){
+            DFS_Visit_OT(adjs, v, visited, ordem);
         }
     }
     ordem.push_back(u);
 }
 
-std::vector<int> ordenacao(std::vector<std::vector<int>> &adjs){
+std::vector<int> dfs(std::vector<std::vector<int>>& adjs, std::vector<bool>& visited){
     std::vector<int> ordem;
-    std::vector<bool> visited(adjs.size(), false);
     for(int i = 0; i < adjs.size(); i++){
         if(!visited[i]){
-            dfs(adjs, i, ordem, visited);
+            DFS_Visit_OT(adjs, i, visited, ordem);
         }
     }
     return ordem;
@@ -54,17 +90,20 @@ int main(void){
     for(;;){
         int n, m; std::cin >> n >> m;
         if(n == 0 && m == 0) break;
-        std::vector<std::vector<int>> adjs(n, std::vector<int>(n, 0));
+        std::vector<std::vector<int>> adjs(n, std::vector<int>(n, false));
         std::vector<bool> visited(n, false);
         for(int i = 0; i < m; i++){
             int a, b; std::cin >> a >> b;
             adjs[a-1][b-1] = 1;
         }
-        std::vector<int> ordem = ordenacao(adjs);
-        for(int i = ordem.size()-1; i >= 0 ; i--){
-            std::cout << ordem[i]+1 << std::endl;
+        if(hasCycle(adjs)){
+            std::cout << "IMPOSSIBLE" << std::endl;
+            continue;
         }
-        
+        std::vector<int> ord = dfs(adjs, visited);
+        for(int i = ord.size()-1; i >= 0; i--){
+            std::cout << ord[i]+1 << std::endl;
+        }
     }
 
 }
