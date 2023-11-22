d3abe9d641dcdf6c8d56512d504fd8869d36a3f0
Micael <mikal2003.sp@gmail.com>
2023-11-22 18:42:35 -0300

    doing the exercise that needed bellman-ford algorithm

---
+++ Semana_8/wormholes.cpp
@@ -0,0 +1,48 @@
+// comecar implementando bellman-ford sobre o grafo dado
+// irei fazer um map de cada aresta com seu respsctivo peso
+// se acaso o grafo conter um ciclo negativo, retornar possible
+// se nao, retornar impossible
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main(void){
+    int n; std::cin >> n;
+    while(n--){
+        int v,e; std::cin >> v >> e;
+        std::map<std::pair<int, int>, int> distances;
+        for(int i = 0; i < e; i++){
+            int a,b,c; std::cin >> a >> b >> c;
+            distances[{a,b}] = c;
+        }
+
+        // bellman-ford
+        std::vector<int> dist(v, 100000000);
+        std::vector<int> ant(v, -1);
+        dist[0] = 0;
+        for(int i = 0; i < v-1; i++){
+            for(auto it = distances.begin(); it != distances.end(); it++){
+                int u = it->first.first;
+                int v = it->first.second;
+                int w = it->second;
+                if(dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
+                    ant[v] = u;
+                }
+            }
+        }
+        bool possible = false;
+        for(auto it = distances.begin(); it != distances.end(); it++){
+            int u = it->first.first;
+            int v = it->first.second;
+            int w = it->second;
+            if(dist[u] + w < dist[v]){
+                std::cout << "possible" << std::endl;
+                possible = true;
+                break;
+            }
+        }
+        if(!possible) std::cout << "not possible" << std::endl;
+    }
+}
