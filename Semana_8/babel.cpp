8628dcba2777ffaf096c2212b70d5e55fe993db6
Micael <mikal2003.sp@gmail.com>
2023-11-24 14:43:13 -0300

    changing babel

--- Semana_8/babel.cpp
+++ Semana_8/babel.cpp
@@ -1,29 +1,21 @@
-// graph modeling
-// each word is a vertex
-// connect two words if they share common language and differ 1st letter
-// cconnect a source vertex to all words that belong to start language
-// connect all words that belong to finish language to sink vertex
-// edge weight = word.size()
-// djisktra
-
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_map>
 
-bool is_there_false_in_visited(std::vector<bool>& visited){
-    for(const auto& v : visited){
-        if(!v) return true;
+bool is_there_false_in_visited(std::vector<bool>& visited) {
+    for (const auto& v : visited) {
+        if (!v) return true;
     }
     return false;
 }
 
-int minimum_distance(std::vector<int> dist, std::vector<bool> visited){
+int minimum_distance(std::vector<int> dist, std::vector<bool> visited) {
     int min = 10000000;
     int min_index = -1;
-    for(int i = 0; i < dist.size(); i++){
-        if(!visited[i] && dist[i] <= min){
+    for (int i = 0; i < dist.size(); i++) {
+        if (!visited[i] && dist[i] <= min) {
             min = dist[i];
             min_index = i;
         }
@@ -31,15 +23,15 @@ int minimum_distance(std::vector<int> dist, std::vector<bool> visited){
     return min_index;
 }
 
-int dijsktra(std::vector<std::vector<int>>& adj_list, std::map<std::pair<int, int>, int>& edges, int start, int finish){
+int dijkstra(std::vector<std::vector<int>>& adj_list, std::map<std::pair<int, int>, int>& edges, int start, int finish) {
     std::vector<int> dist(adj_list.size(), 10000000);
     dist[start] = 0;
     std::vector<bool> visited(adj_list.size(), false);
-    while(is_there_false_in_visited(visited)){
+    while (is_there_false_in_visited(visited)) {
         int u = minimum_distance(dist, visited);
         visited[u] = true;
-        for(const auto& v : adj_list[u]){
-            if(!visited[v] && dist[u] != 10000000 && dist[u] + edges[std::make_pair(u, v)] < dist[v]){
+        for (const auto& v : adj_list[u]) {
+            if (!visited[v] && dist[u] != 10000000 && dist[u] + edges[std::make_pair(u, v)] < dist[v]) {
                 dist[v] = dist[u] + edges[std::make_pair(u, v)];
             }
         }
@@ -47,46 +39,61 @@ int dijsktra(std::vector<std::vector<int>>& adj_list, std::map<std::pair<int, in
     return dist[finish];
 }
 
-int main(void){
-    for(;;){
+int main(void) {
+    for (;;) {
         int n;
         std::string start, finish;
         std::cin >> n;
-        if(n==0) break;
+        if (n == 0) break;
         std::cin >> start >> finish;
-        std::map<std::string, int> graph;
+        std::unordered_map<std::string, int> graph;
         std::map<std::pair<int, int>, int> edges;
-        std::map<std::pair<std::string, std::string>, std::string> languages;
-        std::vector<std::vector<int>> adj_list(n+2);
+        std::unordered_map<std::string, std::vector<std::string>> languages;
+        std::vector<std::vector<int>> adj_list(n + 2);
         graph["start"] = 0;
-        graph["finish"] = n+1;
-        for(int i = 0; i < n; i++){
+        graph["finish"] = n + 1;
+        int cont = 1;
+        for (int i = 0; i < n; i++) {
             std::string lang1, lang2, word;
             std::cin >> lang1 >> lang2 >> word;
-            languages[std::make_pair(lang1, lang2)] = word;
-            graph[word] = i+1;
+            if (!graph.count(word))
+                graph[word] = cont++;
+
+            languages[lang1].push_back(word);
+            languages[lang2].push_back(word);
         }
-        for(const auto& language : languages){
-            if(language.first.first == start || language.first.second == start){
-                adj_list[0].push_back(graph[language.second]);
-                edges[std::make_pair(0, graph[language.second])] = language.second.size();
-            }
-            if(language.first.second == finish || language.first.first == finish){
-                adj_list[graph[language.second]].push_back(n+1);
-                edges[std::make_pair(graph[language.second], n+1)] = 0;
-            }
-            for(const auto& language2 : languages){
-                if(language.first.first == language2.first.second || language.first.second == language2.first.first || language.first.first == language2.first.first || language.first.second == language2.first.second){
-                    if(language.second[0] != language2.second[0]){
-                        adj_list[graph[language.second]].push_back(graph[language2.second]);
-                        edges[std::make_pair(graph[language.second], graph[language2.second])] = language2.second.size();
-                    }
+        for (auto& lang : languages) {
+            auto& words = lang.second;
+            for (int i = 0; i < words.size(); ++i) {
+                std::string word1 = words[i];
+                for (int j = i + 1; j < words.size(); ++j) {
+                    if (word1[0] == words[j][0]) continue;
+
+                    int col_i = graph[word1];
+                    int col_j = graph[words[j]];
+
+                    adj_list[col_i].push_back(col_j);
+                    adj_list[col_j].push_back(col_i);
+                    edges[std::make_pair(col_i, col_j)] = words[j].size();
+                    edges[std::make_pair(col_j, col_i)] = word1.size();
                 }
             }
         }
-        int result = dijsktra(adj_list, edges, 0, n+1);
-        if(result != 10000000)std::cout << result << std::endl;
-        else std::cout << "impossivel" << std::endl;
-        
+        for (auto& word : languages[start]) {
+            int col = graph[word];
+            adj_list[0].push_back(col);
+            edges[std::make_pair(0, col)] = word.size();
+        }
+        for (auto& word : languages[finish]) {
+            int col = graph[word];
+            adj_list[col].push_back(n + 1);
+            edges[std::make_pair(col, n + 1)] = word.size();
+        }
+        int result = dijkstra(adj_list, edges, 0, n + 1);
+        if (result != 10000000)
+            std::cout << result << std::endl;
+        else
+            std::cout << "impossivel" << std::endl;
     }
+    return 0;
 }
